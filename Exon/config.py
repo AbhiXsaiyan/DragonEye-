c0f312e26f981e210871b4a222e9bea20bd639f2
AbhiXsaiyan <111246306+AbhiXsaiyan@users.noreply.github.com>
2022-12-03 05:01:43 +0530

    Update config.py

--- Exon/config.py
+++ Exon/config.py
@@ -38,48 +38,48 @@ def get_user_list(config, key):
 class Config(object):
     LOGGER = True
 
-    API_ID = "983922"
-    API_HASH = "SMSMSMSAKAMAKA"
-    APP_ID = "953922"  # 2nd API_ID
-    APP_HASH = "funssnAjsjaSJns82AjajU"  # 2ns API_HASH
-    ARQ_API_KEY = "TENRCY-KDKSK-MSMSM-OXQYYO-ARQ"
-    BOT_ID = "5408158735"
-    TOKEN = "5458182410:KINGABISHNOI-UM"
-    OWNER_ID = "1452219013"
-    OPENWEATHERMAP_ID = "22322"
-    OWNER_USERNAME = "Abishnoi1M"
-    BOT_USERNAME = "Exon_Robot"
-    SUPPORT_CHAT = "AbishnoiMF"
-    UPDATES_CHANNEL = "Abishnoi_bots"
-    SUPPORT_CHANNEL = "Abishnoi_bots"
-    JOIN_LOGGER = "-1001497222182"
-    EVENT_LOGS = "-1001497222182"
-    ERROR_LOGS = "-1001497222182"
+    API_ID = "23496202"
+    API_HASH = "a1f882ad44ee621df511e060ef4ec719"
+    APP_ID = "12410871"  # 2nd API_ID
+    APP_HASH = "f7a7de22f129870e74bd87e9ef5320bd"  # 2ns API_HASH
+    ARQ_API_KEY = "QBWGHT-LYRDZX-OVEHBS-MEKHOS-ARQ"
+    BOT_ID = "5420854132"
+    TOKEN = "5420854132:AAGYd_CytfcOjenkwsk4DhOUSotMmTiQ62k"
+    OWNER_ID = "5399623240"
+    OPENWEATHERMAP_ID = ""
+    OWNER_USERNAME = "BerlinXbaap"
+    BOT_USERNAME = "Draken_Xbot"
+    SUPPORT_CHAT = "Mysticbots_Support"
+    UPDATES_CHANNEL = "MysticXnetwork"
+    SUPPORT_CHANNEL = "MysticXnetwork"
+    JOIN_LOGGER = "-1001820696533"
+    EVENT_LOGS = "-1001820696533"
+    ERROR_LOGS = "-1001820696533"
 
-    SQLALCHEMY_DATABASE_URI = ""
-    DB_URL = ""
-    MONGO_DB_URL = ""  # needed for any database modules
-    MONGO_URL = ""
-    DB_URL2 = ""  # YOUR MONGO_DB_URI
+    SQLALCHEMY_DATABASE_URI = "postgres://pkkarkxq:RWXrnL_Ggk5lBfnukigJRNDMn88I_tht@peanut.db.elephantsql.com/pkkarkxq"
+    DB_URL = "postgres://pkkarkxq:RWXrnL_Ggk5lBfnukigJRNDMn88I_tht@peanut.db.elephantsql.com/pkkarkxq"
+    MONGO_DB_URL = "mongodb+srv://TaktAsahina99:TaktAsahina99@cluster0.iq3cx2j.mongodb.net/?retryWrites=true&w=majority"  # needed for any database modules
+    MONGO_URL = "mongodb+srv://TaktAsahina99:TaktAsahina99@cluster0.iq3cx2j.mongodb.net/?retryWrites=true&w=majority"
+    DB_URL2 = "mongodb+srv://AranXD99:AranXD99@cluster0.ueowiiw.mongodb.net/?retryWrites=true&w=majority"  # YOUR MONGO_DB_URI
     ARQ_API_URL = "https://arq.hamker.in"
-    BOT_API_URL = "https://api.telegram.org/bot"
+    BOT_API_URL = "https://api.telegram.org/Draken_Xbot"
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""
-    SPAMWATCH_SUPPORT_CHAT = "@AbishnoiMF"
+    SPAMWATCH_API = "6KpHDWB1b~InSrl2UNOV3RZzPHpGEKkYva2DM1l8TB5w8y3Prdc2VPNCHxNHGvHF"
+    SPAMWATCH_SUPPORT_CHAT = "@AnimeChatMystic"
 
-    REDIS_URL = ""
+    REDIS_URL = "redis://Draken99:Draken@99@redis-15058.c8.us-east-1-2.ec2.cloud.redislabs.com:15058/Rajni-free-db"
 
-    DRAGONS = get_user_list("elevated_users.json", "sudos")
-    DEV_USERS = get_user_list("elevated_users.json", "devs")
-    REQUESTER = get_user_list("elevated_users.json", "whitelists")
-    DEMONS = get_user_list("elevated_users.json", "supports")
-    INSPECTOR = get_user_list("elevated_users.json", "1936119750")
-    TIGERS = get_user_list("elevated_users.json", "tigers")
-    WOLVES = get_user_list("elevated_users.json", "whitelists")
+    DRAGONS = get_user_list("elevated_users.json", "1258798381 5519555383")
+    DEV_USERS = get_user_list("elevated_users.json", "5387697107")
+    REQUESTER = get_user_list("elevated_users.json", "")
+    DEMONS = get_user_list("elevated_users.json", "1977550069 5526776892")
+    INSPECTOR = get_user_list("elevated_users.json", "")
+    TIGERS = get_user_list("elevated_users.json", "")
+    WOLVES = get_user_list("elevated_users.json", "")
 
     DONATION_LINK = "https://t.me/Abishnoi1M"
     CERT_PATH = None
@@ -90,13 +90,13 @@ class Config(object):
     WORKERS = 8
     BAN_STICKER = ""
     ALLOW_EXCL = True
-    CASH_API_KEY = "NAI H BRO"
-    TIME_API_KEY = "ABISHNOI"
-    WALL_API = "F-OFF"
-    AI_API_KEY = "LOVEYOU"
+    CASH_API_KEY = ""
+    TIME_API_KEY = "7058IAVQKYTP"
+    WALL_API = ""
+    AI_API_KEY = ""
     BL_CHATS = []
     SPAMMERS = None
-    SPAMWATCH_API = ""
+    SPAMWATCH_API = "6KpHDWB1b~InSrl2UNOV3RZzPHpGEKkYva2DM1l8TB5w8y3Prdc2VPNCHxNHGvHF"
     ALLOW_CHATS = None
     TEMP_DOWNLOAD_DIRECTORY = "./"
     HEROKU_APP_NAME = ""
